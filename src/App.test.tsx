b5e39a33c91d6c2c031c80acb5beb7e9898d7df4
Stephen Haberman <stephen.haberman@gmail.com>
2022-10-12 09:26:21 -0500

    Reproduce.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,26 @@
-import {render} from "@testing-library/react";
-import App from "src/App";
+import { render, screen } from "@testing-library/react";
+import App, { GET_DOGS, GET_CATS } from "src/App";
+import { MockedProvider } from "@apollo/client/testing";
+// import { MockedProvider } from "@homebound/better-apollo-mocked-provider";
 
 describe("App", () => {
-  it("renders", () => {
-    const r = render(<App />);
-  })
-})
+  it("renders", async () => {
+    const dogMock = {
+      request: { query: GET_DOGS },
+      error: new Error("An error occurred"),
+    };
+    const catMock = {
+      request: { query: GET_CATS },
+      result: { data: { cats: [] } },
+    };
+    render(
+      <MockedProvider mocks={[dogMock, catMock]}>
+        <App />
+      </MockedProvider>
+    );
+    expect(await screen.findByText("An error occurred")).toBeDefined();
+  });
+});
+
+beforeEach(() => jest.useFakeTimers());
+afterEach(() => jest.useRealTimers());
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-import { useState } from 'react'
+import { gql, useQuery } from "@apollo/client";
+import { useState } from "react";
+
+export const GET_DOGS = gql`
+  query GetDogs {
+    dogs {
+      id
+      breed
+    }
+  }
+`;
+
+export const GET_CATS = gql`
+  query GetCats {
+    cats {
+      id
+      breed
+    }
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, setState] = useState("");
+  const q1 = useQuery(GET_DOGS, { onError: (e) => setState(e?.message) });
+  console.log("q1", {
+    data: q1.data,
+    loading: q1.loading,
+    error: q1.error?.message,
+  });
+
+  // const q2 = useQuery(GET_CATS, {
+  //   onError: (error) => setState(error?.message),
+  //   skip: false,
+  // });
+  // console.log("q2", {
+  //   data: q2.data,
+  //   loading: q2.loading,
+  //   error: q2.error?.message,
+  // });
 
   return (
-    <h1>Vite + React</h1>
-  )
+    <h1>
+      Vite + React <div>{q1.error?.message}</div>
+    </h1>
+  );
 }
 
-export default App
+export default App;
